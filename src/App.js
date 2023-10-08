84d44cfb1dc7b14e2caa0f3ac4e39234476e3bc9
Farkhunda Saleem <96863402+farkhundaSaleem@users.noreply.github.com>
2023-10-08 11:41:50 +0500

    Update App.js
    Updation in container

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ResponsiveAppBar from './Navbar';
 import './App.css';
 import Button from '@mui/material/Button';
@@ -6,25 +6,33 @@ import Button from '@mui/material/Button';
 const App = () => {
   const [isDaytime, setIsDaytime] = useState(false);
 
+  useEffect(() => {
+    document.body.className = isDaytime ? 'daytime' : 'nighttime';
+  }, [isDaytime]);
+
   const toggleBackground = () => {
     setIsDaytime(!isDaytime);
   };
 
-  document.body.className = isDaytime ? 'daytime' : '';
-  const buttonText = isDaytime ? 'Dark Mode' : 'Light Mode ';
+  const containerClassName = isDaytime ? 'main-container daytime' : 'main-container nighttime';
+
+  const buttonText = isDaytime ? 'Dark Mode' : 'Light Mode';
   return (
     <>
       <ResponsiveAppBar />
-      <div className="main-container">
+      <div className={containerClassName}>
         <div className="round-picture"></div>
         <div className="description">
           <h2>Farkhunda Saleem</h2>
           <p>Front-End Web Developer</p>
-          <p>Passionate about creating interactive web experiences.</p> <p>Here I used useState Hook to Toggle button</p>
+          <p>Passionate about creating interactive web experiences.</p>
+          <p>Here I used useState Hook to Toggle button</p>
           <br />
           <h4>Click here to enable</h4>
           <div>
-            <Button  variant="contained" onClick={toggleBackground}>{buttonText}</Button>
+            <Button variant="contained" onClick={toggleBackground}>
+              {buttonText}
+            </Button>
           </div>
         </div>
       </div>
